f9f57acf9062865390780dac5fd103803544f602
cpfarher <c.pfarher@gmail.com>
2017-07-07 14:38:27 -0300

    Add vue-tooltipster dummy component

---
+++ src/app/vue-tooltipster.spec.js
@@ -0,0 +1,7 @@
+import Vue-tooltipster from './vue-tooltipster.vue';
+
+describe('Vue-tooltipster', () => {
+  it('should be a vue-tooltipster', () => {
+    expect(Vue-tooltipster.name).toEqual('vue-tooltipster');
+  });
+});
---
+++ src/app/vue-tooltipster.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'vue-tooltipster',
+  data() {
+    return {
+      text: 'My new component'
+    };
+  }
+};
+</script>
