3aadffbb54ae2e4206a9f086840a8d7ea2e6951a
cpfarher <c.pfarher@gmail.com>
2017-07-07 15:15:59 -0300

    basic vue tooltipster component with example

--- src/app/Hello.vue
+++ src/app/Hello.vue
@@ -1,5 +1,35 @@
 <template>
-  <vue-tooltipster></vue-tooltipster>
+  <div>
+
+    <h1>Vue JS 2 - Tooltipster componente use example</h1>
+    <div>
+      <vue-tooltipster label="I am the tooltip">
+        <span>Hover me there is a tooltip with label property</span>
+      </vue-tooltipster>
+    </div>
+
+    <div>
+      <vue-tooltipster>
+        <span>Html tooltip content</span>
+        <div slot="content" style="background-color: red">
+          This is html content for render inside tooltip.
+          Use <h1>slot="content"</h1>
+          <div>Also if you need to keep this dialog open, click over text and it not dissapear when mouse goes out! </div>
+        </div>
+      </vue-tooltipster>
+    </div>
+
+    <div>
+      <h2>You can pass options to vue-tooltipster - same object options you can see at http://iamceege.github.io/tooltipster/</h2>
+      <h3>An example with option "trigger: 'click'". Please click over the green svg circle</h3>
+      <vue-tooltipster :label="label" :options="{trigger: 'click'}">
+        <svg height="50" width="50">
+          <circle cx="25" cy="25" r="25" stroke="black" stroke-width="0.5" fill="green" />
+        </svg>
+      </vue-tooltipster>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -8,7 +38,7 @@ export default {
   name: 'Hello',
   data() {
     return {
-      hello: 'Hello World!'
+      label: 'Yea tooltipster option trigger click!'
     };
   },
   components: {
